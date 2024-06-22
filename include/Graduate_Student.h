5860e824bb686c8cd60fb040f5dc78beeacace73
Aaron Ruan <aaron212cn@outlook.com>
2024-06-22 22:11:34 +0800

    add Graduate_Student << output

--- include/Graduate_Student.h
+++ include/Graduate_Student.h
@@ -10,6 +10,11 @@ class Graduate_Student : public Student {
     Graduate_Student() : Student() { projectID = -1; }
 
     Graduate_Student(int id, std::string n, int pID) : Student(id, n) { projectID = pID; }
+
+    friend std::ostream &operator<<(std::ostream &os, const Graduate_Student &gs) {
+        os << "Graduate Student ID: " << gs.id << " Name: " << gs.name << " Project ID: " << gs.projectID;
+        return os;
+    }
 };
 
 #endif // GRADUATE_STUDENT_H

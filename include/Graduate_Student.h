a8a90f75cfb74d9d2df83c6f8908526ea3a77f68
Aaron Ruan <aaron212cn@outlook.com>
2024-06-22 17:28:13 +0800

    use id to map project

--- include/Graduate_Student.h
+++ include/Graduate_Student.h
@@ -1,13 +1,15 @@
 #ifndef GRADUATE_STUDENT_H
 #define GRADUATE_STUDENT_H
 
-#include "Project.h"
 #include "Student.h"
-#include <iostream>
 
 class Graduate_Student : public Student {
   public:
-    Project GStudent_Project;
+    int projectID;
+
+    Graduate_Student() : Student() { projectID = -1; }
+
+    Graduate_Student(int id, std::string n, int pID) : Student(id, n) { projectID = pID; }
 };
 
 #endif // GRADUATE_STUDENT_H

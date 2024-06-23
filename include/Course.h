7f291b66f49b66a60192383f651e87a66f48677b
Aaron Ruan <aaron212cn@outlook.com>
2024-06-23 20:43:08 +0800

    migrate to use SQL

--- include/Course.h
+++ include/Course.h
@@ -2,6 +2,7 @@
 #define COURSE_H
 
 #include <iostream>
+#include <string>
 
 class Course {
   public:
@@ -10,22 +11,31 @@ class Course {
     int credits;      // Number of credits for the course
 
     // Constructor
-    Course() {
-        id = 0;
-        name = "";
-        credits = 0;
-    }
-    Course(int id, const std::string &name, int credits) {
-        this->id = id;
-        this->name = name;
-        this->credits = credits;
-    }
+    Course() : id(0), name(""), credits(0) {}
+
+    Course(int id, const std::string &name, int credits) : id(id), name(name), credits(credits) {}
+
+    // Database format output
+    std::string to_db_string() const { return std::to_string(id) + ", '" + name + "', " + std::to_string(credits); }
 
     // Operator== overload
     bool operator==(const Course &rhs) const { return id == rhs.id; }
+
     friend std::ostream &operator<<(std::ostream &os, const Course &course) {
         os << "Course ID: " << course.id << ", Name: " << course.name << ", Credits: " << course.credits;
         return os;
+    }
+
+    // cin overload
+    friend std::istream &operator>>(std::istream &is, Course &course) {
+        std::cout << "Enter course ID: ";
+        is >> course.id;
+        std::cout << "Enter course name: ";
+        is.ignore();
+        std::getline(is, course.name);
+        std::cout << "Enter course credits: ";
+        is >> course.credits;
+        return is;
     }
 };
 
--- tests/test_score.cpp
+++ tests/test_score.cpp
@@ -5,28 +5,26 @@ TEST(Score, defaultConstructor) {
     Score s;
     ASSERT_EQ(s.courseID, 0);
     ASSERT_EQ(s.studentId, 0);
-    ASSERT_EQ(s.value.percentage, 0);
+    ASSERT_EQ(s.score, 0);
 }
 
 TEST(Score, parameterizedConstructorPercentage) {
     Score s(1, 2, 90);
     ASSERT_EQ(s.courseID, 1);
     ASSERT_EQ(s.studentId, 2);
-    ASSERT_EQ(s.percentage(), 90);
+    ASSERT_EQ(s.score, 90);
 }
 
 TEST(Score, parameterizedConstructorLetter) {
-    Score s(1, 2, LetterGrade::A);
+    Score s(1, 2, 'A');
     ASSERT_EQ(s.courseID, 1);
     ASSERT_EQ(s.studentId, 2);
-    ASSERT_EQ(s.value.letter, LetterGrade::A);
-    ASSERT_EQ(s.percentage(), 100);
+    ASSERT_EQ(s.score, 100);
 }
 
 TEST(Score, parameterizedConstructorPassFail) {
-    Score s(1, 2, PassFail::Pass);
+    Score s(1, 2, "Pass");
     ASSERT_EQ(s.courseID, 1);
     ASSERT_EQ(s.studentId, 2);
-    ASSERT_EQ(s.value.passFail, PassFail::Pass);
-    ASSERT_EQ(s.percentage(), 82);
+    ASSERT_EQ(s.score, 82);
 }

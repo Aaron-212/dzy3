bccb559fd6ba2eb7722f40e3e1a644f96ccb1916
Aaron Ruan <aaron212cn@outlook.com>
2024-06-24 18:42:31 +0800

    add 2nd layer menu, add score interactions, remove Graduate and Project

--- include/Score.h
+++ include/Score.h
@@ -8,24 +8,24 @@
 
 class Score {
   public:
+    int studentID; // The student who obtained the score
     int courseID;  // The course in which the score was obtained
-    int studentId; // The student who obtained the score
     int score;     // The score obtained by the student
 
     // Constructors
-    Score() : courseID(0), studentId(0), score(0) {}
+    Score() : courseID(0), studentID(0), score(0) {}
 
-    Score(int courseID, int studentId, int percentage) : courseID(courseID), studentId(studentId), score(percentage) {}
-    Score(int courseID, int studentId, char letter)
-        : courseID(courseID), studentId(studentId), score(letterToPercentage(letter)) {}
-    Score(int courseID, int studentId, const std::string &pf)
-        : courseID(courseID), studentId(studentId), score(passFailToPercentage(pf)) {}
+    Score(int studentID, int courseID, int percentage) : courseID(courseID), studentID(studentID), score(percentage) {}
+    Score(int studentID, int courseID, char letter)
+        : courseID(courseID), studentID(studentID), score(letterToPercentage(letter)) {}
+    Score(int studentID, int courseID, const std::string &pf)
+        : courseID(courseID), studentID(studentID), score(passFailToPercentage(pf)) {}
 
     // Operator overload
     bool operator==(const Score &other) const { return score == other.score; }
 
     friend std::ostream &operator<<(std::ostream &os, const Score &score) {
-        os << "Course ID: " << score.courseID << ", Student ID: " << score.studentId << ", Score: " << score.score;
+        os << "Student ID: " << score.studentID << ", Course ID: " << score.courseID << ", Score: " << score.score;
         return os;
     }
 
@@ -59,18 +59,17 @@ class Score {
 
     // Method to convert the score object to a database string
     std::string to_db_string() const {
-        std::string result = std::to_string(courseID) + ", " + std::to_string(studentId) + ", " + std::to_string(score);
-        return result;
+        return std::to_string(studentID) + ", " + std::to_string(courseID) + ", " + std::to_string(score);
     }
 
     // cin overload
     friend std::istream &operator>>(std::istream &is, Score &score) {
         std::string type;
+        std::cout << "Enter student ID: ";
+        is >> score.studentID;
         std::cout << "Enter course ID: ";
         is >> score.courseID;
-        std::cout << "Enter student ID: ";
-        is >> score.studentId;
-        std::cout << "Enter score type: ";
+        std::cout << "Enter score type (Percentage/Letter/PassFail): ";
         is >> type;
         if (type == "Percentage") {
             std::cout << "Enter percentage: ";
@@ -82,7 +81,7 @@ class Score {
             score.score = letterToPercentage(grade[0]);
         } else if (type == "PassFail") {
             std::string pf;
-            std::cout << "Enter Pass/Fail: ";
+            std::cout << "Enter Pass or Fail: ";
             is >> pf;
             score.score = passFailToPercentage(pf);
         } else {
